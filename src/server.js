62a35ab5944d7b005e350d87b3d80994f333bf3f
Tumelo Mokoenacd <mokoena532@gmail.com>
2022-11-07 08:16:21 +0200

    add morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
 const port = process.env.port || 3000;
 
 var app  = express();
 
+//Application Middlewares
 app.use(bodyParser.json());
-
+app.use(morgan(':method :url :status :res[content length] - :response-time ms :date[web]'));
 app.use("/api/todos", require("./ApiControllers/todosController"));
 
 app.listen(port, ()=>{
