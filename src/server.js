60903fd4330ac28e3bb2fd4cd4a508721cb47fad
Tumelo Mokoenacd <mokoena532@gmail.com>
2022-11-08 08:03:29 +0200

    Push to remote repo

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const jsonwebtoken = require("jsonwebtoken");
 
+const JWT_SECRET = process.env.jwtSecret;
 const port = process.env.port || 3000;
+
+const accesLogStream = fs.createWriteStream(path.join(__dirname,"Access.log"),{flags:'a'})
+
+const todoRoutes = require("./ApiControllers/todosController");
+const authRoutes = require("./ApiControllers/authController");
+const { Stream } = require("stream");
+
+
 
 var app  = express();
 
 //Application Middlewares
 app.use(bodyParser.json());
-app.use(morgan(':method :url :status :res[content length] - :response-time ms :date[web]'));
-app.use("/api/todos", require("./ApiControllers/todosController"));
+app.use(morgan(':method :url :status :res[content length] - :response-time ms :date[web]',{stream: accesLogStream}));
+app.use("/api/todos", todoRoutes );
+app.use("/api/auth", authRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
